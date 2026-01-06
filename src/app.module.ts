6d9e40b993683541757e5461e006f9355b43718d
unknown <youkata666@gmail.com>
2026-01-06 22:16:23 +0700

    [~] set jwtConfig and R2Config for global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { healthyCheckModule } from './healty_check/healthycheck.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './auth/config/jwt.config';
+import r2Config from './config/r2.config';
 
 @Module({
-  imports: [CommonModule,
-    healthyCheckModule
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // supaya config bisa diakses dari mana saja
+      load: [jwtConfig, r2Config], // Semua config dimuat di sini
+    }),
+
+    CommonModule,
+    healthyCheckModule,
   ],
   controllers: [],
   providers: [],
